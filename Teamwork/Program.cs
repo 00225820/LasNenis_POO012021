d7452acd78fc4539b016df3229d3004989d405dc
Sofia <00225820@gmail.com>
2021-03-12 22:02:18 -0600

    .

--- Teamwork/Program.cs
+++ Teamwork/Program.cs
@@ -2,11 +2,29 @@
 
 namespace Teamwork
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 0, y = 0;
+            Console.WriteLine("Escriba un numero: "); //*Solicitando datos al usuario*//
+            x = Convert.ToInt32(Console.ReadLine()); //*Reconociendo el dato numero 1*//
+
+            Console.WriteLine("Escriba un numero: "); //*Solicitando el segundo dato para poder operarlo*//
+            y = Convert.ToInt32(Console.ReadLine()); //*Reconociendo el dato numero 2*//
+
+            Console.WriteLine($"Sumando los valores de {x} + {y}"); //*Reaalizando el procedimiento*//
+            Console.WriteLine($"El resultado de la suma es:  {adding(x,y)}"); //*Mostrar el resultado que obtuvimos*//
+
+
         }
+
+        public static int adding(int x, int y) //*Funcion para poder operar los datos obtenidos del usuario*//
+        {
+            return x + y; //*Retorna el valor de la operacion*//
+        }
+
     }
+
 }
+
