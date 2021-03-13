8ec3e9319ddc63579b1d4fd95b704d58086cf372
Sofia <00225820@gmail.com>
2021-03-12 21:02:02 -0600

    Starting our first repository

---
+++ Teamwork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Teamwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
